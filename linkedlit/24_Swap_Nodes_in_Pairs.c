e4a1a62f0fb5d00b9991462c82911df6cd98bc22
toufunn <toufunn@users.noreply.github.com>
2017-01-21 15:15:23 -0500

    Update 24_Swap_Nodes_in_Pairs.c
    recommend to use while loop, methods will be simliar as reverse list.

--- linkedlit/24_Swap_Nodes_in_Pairs.c
+++ linkedlit/24_Swap_Nodes_in_Pairs.c
@@ -26,3 +26,60 @@ struct ListNode* swapPairs(struct ListNode* head) {
     }
     return dummy->next;
 }
+
+
+/*2  more complicated */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* swapPairs(struct ListNode* head) {
+    if(!head || !(head->next)) return head;
+    /* Divide the list into odd and even list*/
+    struct ListNode* odd = head;
+    struct ListNode* even = head->next;
+    struct ListNode* headeven = head->next;
+    struct ListNode* cur = even->next;
+
+    int i = 1;
+    while(cur){
+        if(i%2){
+            odd-> next = cur;
+            odd = odd->next;
+        }else{
+            even ->next = cur;
+            even = even->next;
+        }
+        cur = cur ->next;
+        i++;
+    }
+    /*Combined those two lists*/
+    odd = head;
+    even = headeven->next;
+    cur = headeven;
+    i = 1;
+    while(cur){
+      if (i%2){
+          cur->next = odd;
+          if(odd)odd = odd ->next;
+      } else{
+          cur->next = even;
+          if(even)even= even ->next; 
+      }
+      cur = cur->next;
+      i++;
+    }
+    return headeven;
+}
+/*Recursive methods*/
+{
+       if ((head == null)||(head.next == null))
+            return head;
+        ListNode n = head.next;
+        head.next = swapPairs(head.next.next);
+        n.next = head;
+        return n;
+    }
