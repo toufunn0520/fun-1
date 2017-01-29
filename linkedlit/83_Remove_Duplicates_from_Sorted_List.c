e76fba2b06bf565b17c8a68eb3cf73b75c89414d
toufunn <toufunn@users.noreply.github.com>
2017-01-28 21:43:54 -0500

    Create 83_Remove_Duplicates_from_Sorted_List.c

---
+++ linkedlit/83_Remove_Duplicates_from_Sorted_List.c
@@ -0,0 +1,12 @@
+struct ListNode* deleteDuplicates(struct ListNode* head) {
+    if(!head) return head;
+    struct ListNode* cur = head;
+    while(cur && cur->next){
+        if(cur->val == cur->next->val){
+            cur->next = cur->next->next;
+        }else {
+            cur = cur->next;
+        }
+    }
+    return head;
+}
