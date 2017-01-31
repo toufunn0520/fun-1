5f589f3ec8ad5f876dedaaea901bc395ee3baf3b
toufunn <toufunn@users.noreply.github.com>
2017-01-30 22:54:11 -0500

    Update 445_Add_Two_Numbers_II.c

--- linkedlit/445_Add_Two_Numbers_II.c
+++ linkedlit/445_Add_Two_Numbers_II.c
@@ -65,3 +65,69 @@ struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
     return Reverse(head->next);
     
 }
+/*2nd times*/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* reverse(struct ListNode* head){
+    if(!head) return head;
+    struct ListNode* cur = head;
+    struct ListNode* prev = NULL;
+    while(cur){
+        struct ListNode* nex = cur->next;
+        cur->next = prev;
+        prev = cur;
+        cur = nex;
+    }
+    return prev;
+}
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
+    if(!l1) return l2;
+    if(!l2) return l1;
+    
+    struct ListNode* newl1 = reverse(l1);
+    struct ListNode* newl2 = reverse(l2);
+    struct ListNode* result = malloc(sizeof(struct ListNode));
+    struct ListNode* cur = result;
+
+    int sum = 0;
+    int carry = 0;
+    
+    while( newl1 && newl2){
+        cur->next = malloc(sizeof(struct ListNode));
+        sum = (carry + newl1->val + newl2->val)%10;
+        carry = (carry + newl1->val + newl2->val)/10;
+        cur->next->val = sum;
+        cur = cur->next;
+        newl1 = newl1 ->next;
+        newl2 = newl2 ->next;
+    }
+    while(newl1) {
+         cur->next = malloc(sizeof(struct ListNode));
+         sum = (carry + newl1->val)%10;
+         carry = (carry + newl1->val)/10;
+         cur->next->val = sum;
+         cur = cur->next;
+         newl1 = newl1 ->next;
+    }
+    while(newl2){
+         cur->next = malloc(sizeof(struct ListNode));
+         sum = (carry + newl2->val)%10;
+         carry = (carry + newl2->val)/10;
+         cur->next->val = sum;
+         cur = cur->next;
+         newl2 = newl2 ->next;
+    }
+    if (carry) {
+        cur->next = malloc(sizeof(struct ListNode));
+        cur->next->val = carry;
+        cur = cur->next;
+    }
+    
+    cur->next = NULL;
+    return reverse(result->next);
+}
