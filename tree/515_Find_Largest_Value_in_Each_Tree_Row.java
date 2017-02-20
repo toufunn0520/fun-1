5c60a7a1624a9aff6ecb97a69afde200c671043d
toufunn <toufunn@users.noreply.github.com>
2017-02-20 11:24:53 -0500

    Create 515_Find_Largest_Value_in_Each_Tree_Row.java
    level order BFS

---
+++ tree/515_Find_Largest_Value_in_Each_Tree_Row.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<Integer> largestValues(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        if (root==null) return res;
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()){
+            int size = q.size();
+            int max = Integer.MIN_VALUE;
+            for(int i =0; i< size; i++){
+                TreeNode cur =  q.poll();
+                max = Math.max(max, cur.val);
+                if(cur.left!=null) q.add(cur.left);
+                if(cur.right!=null) q.add(cur.right);
+            }
+            res.add(max);
+        }
+        
+        return res;
+    }
+}
