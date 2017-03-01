9535edd94dd9cd30f487545d9259337e08efe3a3
toufunn <toufunn@users.noreply.github.com>
2017-03-01 00:05:04 -0500

    Create 405_Convert_a_Number_to_Hexadecimal.c
    convert into unsigned long long num

---
+++ bit/405_Convert_a_Number_to_Hexadecimal.c
@@ -0,0 +1,29 @@
+
+char map[] = {'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'};
+/*num are positive*/
+void internal(unsigned long long num, char *res, int* index){
+    if(!res) return;
+    int cur = 0;
+    while( num && (*index)){
+            cur = num & 15;
+            res[(*index)-1] = map[cur];
+            num = num>>4;
+            (*index)--;
+    }
+    return;
+}
+
+char* toHex(int num) {
+    char *res = malloc(sizeof(char)* 9);
+    memset(res, 0, 9);
+    int index = 8;
+    if(num == 0) {
+        res[7] ='0';
+        index = 7;
+        goto out;
+    }
+    internal(num, res, &index);
+    //printf("%d res=%c len%d\n", index, res[index], strlen(&(res[index])));
+out:
+    return &(res[index]);
+}
