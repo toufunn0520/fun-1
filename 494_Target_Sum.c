d085d0bdaf7e27813c1e7b4389a35bfd62e889ed
toufunn <toufunn@users.noreply.github.com>
2017-03-18 15:01:05 -0400

    Update 494_Target_Sum.c

--- 494_Target_Sum.c
+++ 494_Target_Sum.c
@@ -24,3 +24,47 @@ int findTargetSumWays(int* nums, int numsSize, int S) {
     helper(nums, numsSize, S, 0, 0,s, &result);
     return result;
 }
+
+/*DP solution*/
+/*dp[n][s] = dp[n-1][s-a[n-1]]+ dp[n-1][s+a[n-1]]
+ * Total sum of the array is B
+ * since the second demention is half negative from -B to B so we need to map it to 0->2B
+ * Init dp[0][a[0]] = dp[0][-a[0]] = 1
+ * If a[0] = need to init dp[a][a[0]] = 2 
+ */
+int findTargetSumWays(int* nums, int numsSize, int S) {
+    
+    if(!nums || !numsSize) return 0;
+    int s = 0;
+    for (int i = 0; i<numsSize; i++) {
+        s += nums[i];
+    }
+    if(S>abs(s)) return 0;
+
+    /*Init dp array*/
+    int secondSize = 1 + (s<<1) ;
+    int **dp = malloc(sizeof(int*) *numsSize);
+    for (int i =0; i<numsSize; i++) {
+        dp[i] = malloc(sizeof(int) * secondSize);
+        for (int j = 0; j<secondSize; j++) {
+            dp[i][j] = 0;
+        }
+    }
+    dp[0][s+nums[0]] =1;
+    dp[0][s-nums[0]] +=1;
+            
+
+    for (int i = 1; i< numsSize; i++) {
+        for (int j = 0; j<secondSize; j++) {
+            /*Check Range pruning unavailable result*/
+            if (j>=nums[i]){
+                dp[i][j] += dp[i-1][j-nums[i]];
+            }
+            if(j+nums[i]<secondSize){
+                dp[i][j] += dp[i-1][j+nums[i]];
+            }
+        }
+    }
+    
+    return dp[numsSize-1][S+s];
+}
