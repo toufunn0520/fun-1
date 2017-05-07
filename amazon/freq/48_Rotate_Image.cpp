6d9ee664b9a20b6713fb98524536117c9a18d9a1
toufunn <toufunn@users.noreply.github.com>
2017-05-07 17:33:21 -0400

    Create 48_Rotate_Image.cpp
    (1) line swap
    (2) symmetric swap

---
+++ amazon/freq/48_Rotate_Image.cpp
@@ -0,0 +1,21 @@
+void rotate(int** matrix, int matrixRowSize, int matrixColSize) {
+    if(!matrix && matrixRowSize!=matrixColSize) return matrix;
+    int len = matrixRowSize;
+    // 1st reverse up to downs
+    for(int i =0; i<len/2; i++){
+        int *tmp = matrix[i];
+        matrix[i] = matrix[len-1-i];
+        matrix[len-i-1] = tmp;
+        
+    }
+    //2nd swap symmetry
+    for(int i =0; i<len; i++){
+        for(int j =i+1; j<len; j++){
+            int tmp = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = tmp;
+        }
+    }
+    
+    return matrix;
+}
