a9c27eda2ac78e3e4f2326a1364e86e737a465e2
toufunn <toufunn@users.noreply.github.com>
2017-06-15 22:50:48 -0400

    Create 398_Random_Pick_Index.cpp

---
+++ FACEBOOK/398_Random_Pick_Index.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> n;
+    Solution(vector<int> nums) {
+        n = nums;
+    }
+    
+    int pick(int target) {
+        int count = 0, res = -1;
+        for (int i = 0; i < n.size(); i++) {
+            if (n[i] == target) {
+                if(++count == 1) res=i;
+                else{
+                    if(!(rand()%count)) {res = i;} 
+                }
+            }
+        }
+        return res;
+    }
+};
