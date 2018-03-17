df39e9ecaa1a42ebc7266d1974ec5b0e526676f6
toufunn <toufunn@users.noreply.github.com>
2018-03-17 17:56:17 -0400

    Update 200_Number_of_Islands.cpp

--- amazon/freq/200_Number_of_Islands.cpp
+++ amazon/freq/200_Number_of_Islands.cpp
@@ -26,3 +26,40 @@ int numIslands(char** grid, int gridRowSize, int gridColSize) {
     }
     return res;
 }
+
+
+class Solution {
+public:
+    //mark connected to 0
+    void dfs(vector<vector<char>>& grid, int i , int j){
+        int col = grid[0].size();
+        int row = grid.size();
+        if(i<0 || i>=row || j<0 || j>=col) return;
+        if (grid[i][j] == '1') {
+            grid[i][j] = '0';
+            dfs(grid, i-1,j);
+            dfs(grid, i,j-1);
+            dfs(grid, i,j+1);
+            dfs(grid, i+1,j);
+        }
+    }
+    
+    int numIslands(vector<vector<char>>& grid) {
+        int res = 0;
+        //if(grid == nullptr  && grid.empty()) return res;
+        if(grid.empty()) return res;
+
+        int col = grid[0].size();
+        int row = grid.size();
+        
+        for (int i = 0; i<row; i++) {
+            for (int j = 0; j<col; j++) {
+                if (grid[i][j] == '1') {
+                    res++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return res;
+    }
+};
